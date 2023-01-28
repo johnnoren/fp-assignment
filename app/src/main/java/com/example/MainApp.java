6c91b29965235da34bfc44149b25cae58b56d73f
John Norén <super.happy.email.fun@gmail.com>
2023-01-28 18:36:22 +0100

    Everything working, starting point for new javafx app

--- app/src/main/java/com/example/MainApp.java
+++ app/src/main/java/com/example/MainApp.java
@@ -11,10 +11,10 @@ public class MainApp extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("/scene.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/com/example/scene.fxml"));
 
 		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+		scene.getStylesheets().add(getClass().getResource("/com/example/styles.css").toExternalForm());
 
 		stage.setTitle("JavaFX and Gradle");
 		stage.setScene(scene);
